99f906f2a7dc02febb52d1900808bb35b12b7e02
IVKrylova <ivk.lis@yandex.ru>
2023-01-30 12:18:19 +0300

    feat: add styles of the header

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,23 @@
 import * as utils from './modules/utils.js';
+import {
+  inputRangeSumFormHeader,
+  valueRangeSumFormHeader,
+  inputRangeTimeFormHeader,
+  valueRangeTimeFormHeader,
+} from './modules/constants.js';
 
 utils.isWebp();
+
+/* form-credit */
+inputRangeSumFormHeader.addEventListener("change", function() {
+  const value = this.value.toString().slice(0, (this.value.length - 3)) + ' 000 ₽';
+  valueRangeSumFormHeader.textContent = value;
+});
+
+inputRangeTimeFormHeader.addEventListener("change", function() {
+  if (this.value === '3' || this.value === '3') {
+    valueRangeTimeFormHeader.textContent = `${this.value} дня`;
+  } else {
+    valueRangeTimeFormHeader.textContent = `${this.value} дней`;
+  }
+});
---
+++ src/js/modules/constants.js
@@ -0,0 +1,5 @@
+/* form credit */
+export const inputRangeSumFormHeader = document.querySelector('.form-credit__range_sum');
+export const valueRangeSumFormHeader = document.querySelector('.form-credit__range-value_sum');
+export const inputRangeTimeFormHeader = document.querySelector('.form-credit__range_time');
+export const valueRangeTimeFormHeader = document.querySelector('.form-credit__range-value_time');
